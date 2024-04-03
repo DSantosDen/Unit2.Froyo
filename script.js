c203c5c81eb8329704029f8dc6868ebbced5b471
DSantosDen <51485458+DSantosDen@users.noreply.github.com>
2024-04-03 00:52:16 -0600

    comments added

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const stringArray = userInputString.split(",");
 //Empty object initialized
 const flavorCounter = {};
 /*Loop iterates over items,
-each element during the loop using a logical OR
+each element during the loop uses a logical OR
 operator. If value exists and has a value, use that value.
 Otherwise, use 0 and hold the values
 */
@@ -27,14 +27,24 @@ Show a table in the console listing
 how many of each flavor they have listed
 Version 2
 */
-//Empty object for version 2 initialized
+//Empty dictionary for version 2 initialized
 const flavorCounterVer2 = {};
 /*Loop iterates over items,
 each element during the loop add a counter
 and holds the values
 */
 for (i = 0; i < stringArray.length; i++) {
+/*currentFlavor holds the flavor for each loop iteration,
+stringArray[i] accesses each element of the array starting
+at zero and for the length specified inside the for loop condition.
+*/
   let currentFlavor = stringArray[i];
+
+/*
+if conditional initialized.
+if current flavor is in the object using shorthand add one and store,
+else only assign 1 to the counter
+*/
   if (currentFlavor in flavorCounterVer2) {
     flavorCounterVer2[currentFlavor] += 1;
   } else {
